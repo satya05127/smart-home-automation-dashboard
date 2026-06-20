89e9fd1379eabcae5ed45c5ce2221e618a1a10a9
satya05127 <osrisatyavanisatya@gmail.com>
2026-06-20 22:34:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let devices = {
+    light:false,
+    fan:false,
+    ac:false
+};
 
+function toggleDevice(device){
+
+    devices[device] = !devices[device];
+
+    document.getElementById(device + "-status").innerText =
+        devices[device] ? "ON" : "OFF";
+}
+function toggleDevice(device){
+
+    devices[device] = !devices[device];
+
+    document.getElementById(device + "-status").innerText =
+    devices[device] ? "ON" : "OFF";
+
+    let activeCount = Object.values(devices)
+        .filter(status => status).length;
+
+    document.getElementById("active").innerText =
+        "Active Devices : " + activeCount;
+}
